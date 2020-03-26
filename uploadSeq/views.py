56cbde9bd6e16b77c7a92f02ca63ccae4e2a4576
HowardChao <ntueeb05howard@gmail.com>
2020-03-27 06:22:17 +1100

    Add modal to fasta file sibmission

--- uploadSeq/views.py
+++ uploadSeq/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.views.generic.detail import DetailView
 from django.template import RequestContext
+from django.urls import reverse
 from . import forms
 from . import models
 
@@ -20,15 +21,22 @@ from . import models
 #         return render(request, 'uploadSeq/upload_sequences_page.html', {'form': form})
 
 def upload_file(request):
-    if request.method == 'POST' and 'fasta-files-upload' in request.POST:
+    if request.method == 'POST' and 'fasta-files-upload-modal' in request.POST:
         form = forms.DocumentForm(request.POST, request.FILES)
+        print("form: ", form)
+        print("form.is_valid(): ", form.is_valid())
         if form.is_valid():
             doc = models.Document(fastafile = request.FILES['fastafile'])
             doc.save()
+            return HttpResponseRedirect(reverse('uploadSeq-upload'))
     else:
         form = forms.DocumentForm()
+    documents = models.Document.objects.all()
+    # for document in documents:
+    #     document.delete()
     return render(
         request,
         'uploadSeq/upload_sequences_page.html',
-        {'form': form}
+        {'documents': documents, 'form': form}
     )
+    # return JsonResponse({'tmp': ''})
