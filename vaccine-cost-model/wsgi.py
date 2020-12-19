aa8de12cc3a7b72264ce00172e59568d5dbb5969
Kuan-Hao Chao <ntueeb05howard@gmail.com>
2020-12-19 14:38:38 +0800

    Update .gitignore

--- vaccine-cost-model/wsgi.py
+++ vaccine-cost-model/wsgi.py
@@ -9,7 +9,27 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 import sys
+import site
 from django.core.wsgi import get_wsgi_application
+
+python_home = '/ssd/Howard/vaccine_env'
+#activate_this = python_home + '/bin/activate_this.py'
+#execfile(activate_this, dict(__file__=activate_this))
+
+# Calculate path to site-packages directory.
+
+python_version = '.'.join(map(str, sys.version_info[:2]))
+site_packages = python_home + '/lib/python%s/site-packages' % python_version
+site.addsitedir(site_packages)
+
+# Remember original sys.path.
+
+prev_sys_path = list(sys.path)
+
+# Add the site-packages directory.
+
+site.addsitedir(site_packages)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vaccine-cost-model.settings')
 
